4e6e5609ecd8ab609b232ea93787452911f9e64e
FlyingCat13 <hoang_pt@outlook.com>
2022-05-28 20:03:17 +1000

    Create URLShortener app & URLRedirect model

---
+++ URLShortener/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.crypto import get_random_string
+
+class URLRedirect(models.Model):
+    original_URL = models.URLField()
+    short_URL_suffix = models.CharField(max_length=6, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    click_count = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.short_URL_suffix:
+            new_suffix = get_random_string(length=6)
+
+            while URLRedirect.objects.filter(short_URL_suffix = new_suffix).exists():
+                new_suffix = get_random_string(length=6)
+            
+            self.short_URL_suffix = new_suffix
+
+        super().save(self, *args, **kwargs)
